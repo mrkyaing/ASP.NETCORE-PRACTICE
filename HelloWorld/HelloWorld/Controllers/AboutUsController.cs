f9ffc851cd05fa742bf13b6f0146df08dedae66d
mrkyaing <mr.kyaing7@gmail.com>
2023-01-29 15:49:41 +0630

    initial code

---
+++ HelloWorld/HelloWorld/Controllers/AboutUsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
