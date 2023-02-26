1140dad8dab5e49c6aa41d4bf96fe6b291996231
mrkyaing <mr.kyaing7@gmail.com>
2023-02-26 10:44:20 +0630

    demo for JQuery & AJAX

---
+++ HelloWorld/WorkOutMVC/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WorkOutMVC.Models;
+namespace WorkOutMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string TellMeDateTime()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public IActionResult MakeOrder()=>View();
+
+        [HttpPost]
+        public JsonResult MakeOrder(OrderModel orderModel)
+        {
+            orderModel.OrderDate=DateTime.Now.ToString();
+            //ViewBag.Order = $"Order Info:{orderModel.Id} and your order status is {orderModel.Status} and your order date is :{orderModel.OrderDate}";
+            return Json(orderModel);
+        }
+    }
+}
---
+++ HelloWorld/WorkOutMVC/Models/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkOutMVC.Models
+{
+    public class OrderModel
+    {
+        public string Id { get; set; }
+        public string Status { get; set; }
+        public string OrderDate { get; set; }
+        public int Quantity { get; set; }
+    }
+}
